4c79dc8a806f103d140e058397eb73167236243f
Jordan Thomas <me@jordanthom.as>
2016-03-27 00:36:32 +0000

    Add super duper animations!

--- beta/css/coolStuff.js
+++ beta/css/coolStuff.js
@@ -1,95 +1,60 @@
 
-	/* Smooth scrolling */
-	$(document).ready(function(){
-		$('a[href^="#"]').on('click',function (e) {
-		    e.preventDefault();
+/* 
+	Animations and other JS tricks 
+	http://jordanthom.as 
+	2016 Jordan Thomas
+*/
 
-		    var target = this.hash;
-		    var $target = $(target);
+/* Smooth scrolling */
 
-		    $('html, body').stop().animate({
-		        'scrollTop': $target.offset().top
-		    }, 900, 'swing', function () {
-		        window.location.hash = target;
-		    });
-		});
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
 	});
+});
+
+/* Scroll effects */
+
+$(window).scroll(function (event) {
+    
+    var scroll = $(window).scrollTop();
+
+	if (scroll > 280) {
+	  	$("header").css("padding-top", "20px");
+	  	$("header .links #menu").css("top", "57px");
+    }
+    if (scroll < 280) {
+	  	$("header").css("padding-top", "40px");
+	  	$("header .links #menu").css("top", "80px");
+    }
+
+});
 
 
-	/* SVG colour changing */
+/* Animate on visible */
 
+$(window).on('scroll', function(){
 
-		var changeMenuColor = {
+	if ($(".about").visible()){
+		$(".about p").css("display", "block");
+	}
 
-		  //change these to elements to use at waypoints
-		  toTrack: "section",
-		  tracked: {},
+	if ($(".education .container").visible()){
+		$(".education .educard").css("display", "block");
+	}
 
-		  // utility function to check if its within the tracked area
-		  inObject: function(val){
-		    var self = this;
-		    var ret = false;
-		    var keys= Object.keys(this.tracked);
-		    keys.push(keys[keys.length-1]);
-		    var index = 0;
-		    $.each(this.tracked, function(k,v){
-		      var next = self.tracked[keys[index+1]];
-		      if (v<=val && val<=next){
-		        ret =  k;
-		      }
-		       index++;
-		    });
-		    return ret;
-		  },
+	if ($(".contact .container").visible()){
+		$(".contact .contactcircle").css("display", "block");
+	}
 
-		  //sets up tracked elements for events
-		  init : function(){
-		  var self = this;
-		   //create a collecion of tracked offsets
-		    $(this.toTrack).each(function(){
-		       self.tracked[$(this).attr("id")] = $(this).offset().top;
-		    });
+ });
 
-		    //on scroll, we compare current position and tracked offsets,
-		    // if they match we trigger the isOnElement.changeMenuColor event
-		    // on the element
-		    $(document).on("scroll", function(){
-		        var currentPosition = $(this).scrollTop();
-		        var trackFound = self.inObject(currentPosition);
-		        if (trackFound){
-		          $("#"+trackFound).trigger("isOnElement.changeMenuColor");
-		        }
-		    });
-		  }
-		}
-
-		//on dom load we initalize the changeMenuColor operation
-		//now we just need to listen for the evemt amd do whatrever
-		$(function(){
-			changeMenuColor.init();
-
-			$("#welcome").on("isOnElement.changeMenuColor", function(){
-			  $("header .logo path").css("fill", "rgb(83,87,96)");
-			  $("header .links svg path").css("fill", "rgb(83,87,96)");
-			});
-
-			$("#projects").on("isOnElement.changeMenuColor", function(){
-			  $("header .logo path").css("fill", "rgb(83,87,96)");
-			  $("header .links svg path").css("fill", "rgb(83,87,96)");
-			});
-
-			$("#about").on("isOnElement.changeMenuColor", function(){
-			  $("header .logo path").css("fill", "rgb(83,87,96)");
-			  $("header .links svg path").css("fill", "rgb(83,87,96)");
-			});
-
-			$("#education").on("isOnElement.changeMenuColor", function(){
-			  $("header .logo path").css("fill", "rgb(255,255,255)");
-			  $("header .links svg path").css("fill", "rgb(255,255,255)");
-			});
-
-			$("#contact").on("isOnElement.changeMenuColor", function(){
-			  $("header .logo path").css("fill", "rgb(83,87,96)");
-			  $("header .links svg path").css("fill", "rgb(83,87,96)");
-			});
-		});
